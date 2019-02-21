6c591a654a7898431c4e4c5344182fa0e6be985b
Jonathan Wilkinson <jonny041187@gmail.com>
2019-02-21 14:05:08 +0000

    Launch polybar on multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=HDMI-1 polybar --reload topbar &
+MONITOR=HDMI-1 polybar topbar &
+MONITOR=eDP-1 polybar topbar &
+MONITOR=DP-2 polybar topbar &
 
 #polybar topbar &
 if type "xrandr"; then
